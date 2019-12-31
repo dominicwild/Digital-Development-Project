67d0b2c54bdb368612f8564fac089024187bab0b
dwild8 <dwild8@dxc.com>
2019-12-31 05:32:13 +0000

    Moved Server Folder Into Src
    Moved because react will not import any JS files outside of src and the models are needed to input data into the database.

---
+++ src/server/models/SkillModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("../mongo");
+
+const SkillSchema = new mongoose.Schema({
+  skill: { type: String, required: true, unique: true, trim: true }
+});
+
+const Skill = mongoose.model("Skill", SkillSchema);
+Skill.init()
+
+function create(skill) {
+  return Skill.create(skill);
+}
+
+function get(id) {
+  return Skill.findById(id).exec();
+}
+
+function update(skill) {
+    return Skill.update({_id: skill.id}, skill);
+}
+
+function destroy(id) {
+  return Skill.findByIdAndDelete(id);
+}
+
+module.exports = { create, get, update, destroy };
---
+++ src/server/mongo.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const config = require('./config');
+
+
+// const mongoUri = encodeURI(`mongodb://${config.host}:${config.key}@${config.host}:${config.port}/admin?ssl=true`)
+const mongoUri = "mongodb://localhost:C2y6yDjf5%2FR%2Bob0N8A7Cgv30VRDJIWEHLM%2B4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw%2FJw%3D%3D@localhost:10255/test?ssl=true"//config.connectionString
+
+function connect(){
+    console.log(mongoUri)
+    //mongoose.connect(mongoUri,{useNewUrlParser: true, useUnifiedTopology: true})
+}
+
+module.exports =  mongoose
