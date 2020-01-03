ca5e5544bc0fb992c5c6e8d95c7f9f6b477f1b0c
dwild8 <dwild8@dxc.com>
2020-01-03 10:17:47 +0000

    Code Cleanup
    Removed unnecessary example code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./css/App.css";
 import "./css/Sidebar.css";
 import "bootstrap";
@@ -15,14 +14,6 @@ require("./StringPrototypes");
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      name: "",
-      greeting: "",
-      DDRList: this.randDates()
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   randDates() {
@@ -34,19 +25,6 @@ class App extends Component {
     }
 
     return dates;
-  }
-
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-      .then(response => response.json())
-      .then(state => {
-        this.setState(state);
-      });
   }
 
   route() {
